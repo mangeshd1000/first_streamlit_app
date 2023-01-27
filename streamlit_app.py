3c6663ec591f5555132d581e0776381a8e667e30
mangeshd1000 <123717428+mangeshd1000@users.noreply.github.com>
2023-01-27 11:32:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ streamlit.text('🥑🍞 Avocado toast')
 streamlit.header('🍌🍍 Build your own fruit Smoothie 🍐🍑')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.pandas(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
